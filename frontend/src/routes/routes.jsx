4c3c815a97ed89fb0b75be5d44b84e5177bdd4d2
Rauan <rauanconceicao789@gmail.com>
2024-09-19 12:41:27 -0300

    ajuste nas rotas

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,6 +1,6 @@
 import { PageDeliveryMan } from "../pages/Deliveryman/delivery";
 import { PageClient } from "../pages/cliente/cliente";
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from "../pages/login/login";
 import RegisterCLiente from "../pages/registro/registroClient";
 import LoginEntregador from "../pages/loginEntrgador/loginEntregador";
@@ -11,15 +11,18 @@ import { PrivateDeliMan } from "./private";
 export const RoutesComponest = () => {
     return (
         <Routes>
-            <Route path="/loginClient" element={<LoginPage/>} />
-            <Route  path="/registro/client" element={<RegisterCLiente/>} />
-
-            <Route   path="/login/entregador" element={<LoginEntregador/>} />
-            <Route  path="/registro/Entregador" element={<RegisterEntregador/>} />
-
-            <Route path='/deliveryman' element={<PrivateDeliMan Component={PageDeliveryMan}/>} />
-            <Route  path="/client"  element={<Private Component={PageClient}  />}/>
+            {/* Rotas públicas */}
+            <Route path="/loginClient" element={<LoginPage />} />
+            <Route path="/registro/client" element={<RegisterCLiente />} />
+            <Route path="/login/entregador" element={<LoginEntregador />} />
+            <Route path="/registro/entregador" element={<RegisterEntregador />} />
+            
+            {/* Rotas privadas */}
+            <Route path="/deliveryman" element={<PrivateDeliMan Component={PageDeliveryMan} />} />
+            <Route path="/client" element={<Private Component={PageClient} />} />
+            
+            {/* Rota padrão - redireciona para o login do cliente */}
+            <Route path="*" element={<Navigate to="/loginClient" />} />
         </Routes>
-    )
-}
-
+    );
+};
